3387e5b16a67b9dec11c08f0c073e81680295463
atahar <ataharsk10@gmail.com>
2024-01-05 19:51:10 +0530

    Setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI-Indian Cooking Chatbot',
+    version= '0.0.0',
+    author= 'Sk Atahar Ali',
+    author_email= 'ataharsk10@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
